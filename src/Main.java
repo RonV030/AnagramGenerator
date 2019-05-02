acce7d7835722b4ff01bc9198665556e48f14677
Ronny <rv48@hotmail.de>
2019-05-02 21:02:27 +0200

    Awesomeness

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is my anagram generator");
+        System.out.println("this is my awesome anagram generator");
     }
 }
